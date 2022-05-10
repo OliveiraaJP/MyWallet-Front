3cd85a3aa2fb67a66ee37e9f88927193012d2c85
Joao Paulo <oliveirajpufrj@gmail.com>
2022-05-10 15:25:11 -0300

    feat: post transaction

--- src/pages/Entry/index.jsx
+++ src/pages/Entry/index.jsx
@@ -1,16 +1,60 @@
-import {$container, $header, $input, $button } from "./styles"
+import { $container, $header, $input, $button } from "./styles";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../../contexts/userContext";
+import axios from "axios";
 
-function Entry(){
-    return(
-        <$container>
-        <$header><p>Nova entrada</p></$header>
-        <form>
-            <$input placeholder="Valor"></$input>
-            <$input placeholder="Descrição"></$input>
-            <$button><p> Salvar entrada </p></$button>
-        </form>
-        </$container>
-    )
+function Entry() {
+  const navigate = useNavigate();
+
+  const [value, setValue] = useState("");
+  const [description, setDescription] = useState("");
+  const { userToken } = useContext(UserContext);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${userToken.token}`,
+    },
+  };
+
+  async function sendEntry(event) {
+    event.preventDefault();
+    const body = {
+      description,
+      type: "entry",
+      value: parseFloat(value),
+    };
+
+    try {
+        await axios.post("http://localhost:5000/transactions", body, config)
+        console.log("postou entrada ->" + body);
+        navigate ("/")
+    } catch (error) {
+        console.log("error entry", error);
+    }
+  }
+
+  return (
+    <$container>
+      <$header>
+        <p>Nova entrada</p>
+      </$header>
+      <form onSubmit={sendEntry}>
+        <$input
+          placeholder="Valor"
+          required
+          onChange={(e) => setValue(e.target.value)}
+        ></$input>
+        <$input
+          placeholder="Descrição"
+          required
+          onChange={(e) => setDescription(e.target.value)}
+        ></$input>
+        <$button type="submit">
+          <p> Salvar entrada </p>
+        </$button>
+      </form>
+    </$container>
+  );
 }
 
 export default Entry;
--- src/pages/Exit/indes.jsx
+++ src/pages/Exit/indes.jsx
@@ -1,16 +1,60 @@
-import {$container, $header, $input, $button } from "./styles"
+import { $container, $header, $input, $button } from "./styles";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../../contexts/userContext";
+import axios from "axios";
 
-function Exit(){
-    return(
-        <$container>
-        <$header><p>Nova saída</p></$header>
-        <form>
-            <$input placeholder="Valor"></$input>
-            <$input placeholder="Descrição"></$input>
-            <$button><p> Salvar saída </p></$button>
-        </form>
-        </$container>
-    )
+function Exit() {
+  const navigate = useNavigate();
+
+  const [value, setValue] = useState("");
+  const [description, setDescription] = useState("");
+  const { userToken } = useContext(UserContext);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${userToken.token}`,
+    },
+  };
+
+  async function sendExit(event) {
+    event.preventDefault();
+    const body = {
+      description,
+      type: "exit",
+      value: parseFloat(value),
+    };
+
+    try {
+        await axios.post("http://localhost:5000/transactions", body, config)
+        console.log("postou saída ->" + body);
+        navigate ("/")
+    } catch (error) {
+        console.log("error exit", error);
+    }
+  }
+
+  return (
+    <$container>
+      <$header>
+        <p>Nova saída</p>
+      </$header>
+      <form onSubmit={sendExit}>
+        <$input
+          placeholder="Valor"
+          required
+          onChange={(e) => setValue(e.target.value)}
+        ></$input>
+        <$input
+          placeholder="Descrição"
+          required
+          onChange={(e) => setDescription(e.target.value)}
+        ></$input>
+        <$button type="submit">
+          <p> Salvar saída </p>
+        </$button>
+      </form>
+    </$container>
+  );
 }
 
 export default Exit;
